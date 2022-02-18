a1b495008f094ed4bb6d69518ef765f5e6a7168c
MatGawron <gawmath007@gmail.com>
2022-02-18 14:53:05 +0100

    oporawa szarych warningow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-#import re
+# import re
 import tkinter as ttk
 from tkinter import *
 
@@ -14,8 +14,10 @@ class Model:
     def __init__(self, plansza):
         self.plansza = plansza
 
+
 global board
 board = [[" " for x in range(3)] for y in range(3)]
+
 
 class View(ttk.Frame):
     root = ttk.Tk()
@@ -35,24 +37,24 @@ class View(ttk.Frame):
     label2.grid(row=0, column=2)
 
 # Create Buttons
-    b1 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b1, 0, 0))
-    b2 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b2, 0, 1))
-    b3 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b3, 0, 2))
-    b4 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b4, 1, 0))
-    b5 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b5, 1, 1))
-    b6 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b6, 1, 2))
-    b7 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b7, 2, 0))
-    b8 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b8, 2, 1))
-    b9 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white", font="Times 15 bold",
-                command=lambda: changeVal(b9, 2, 2))
+    b1 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b1, 0, 0))
+    b2 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b2, 0, 1))
+    b3 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b3, 0, 2))
+    b4 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b4, 1, 0))
+    b5 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b5, 1, 1))
+    b6 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b6, 1, 2))
+    b7 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b7, 2, 0))
+    b8 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b8, 2, 1))
+    b9 = Button(root, text="", height=4, width=8, bg="black", activebackground="white", fg="white",
+                font="Times 15 bold", command=lambda: changeVal(b9, 2, 2))
     b1.grid(row=2, column=0)
     b2.grid(row=2, column=1)
     b3.grid(row=2, column=2)
