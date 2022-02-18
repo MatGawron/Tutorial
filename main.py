56b4da72ca1798665b22cdbbc450aa367581cece
MatGawron <gawmath007@gmail.com>
2022-02-18 13:47:09 +0100

    usunięcie aktualnie nie działających części programu aby był bardziej przejrzysty

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 #import re
 import tkinter as ttk
 from tkinter import *
-#import tkinter.messagebox
 
 
 class Model:
@@ -15,27 +14,6 @@ class Model:
     def __init__(self, plansza):
         self.plansza = plansza
 
-    @property
-# pusta plansza do gry
-    def grid(self):
-        return self.grid
-
-    @grid.setter
-    def grid(self, value):
-        self.grid.set_name = [[self.Empty for x in range(3)] for x in range(3)]
-
-    def save(self):
-        """
-    Save the game progress into a file
-    :return:
-        """
-        filepath = "game_prg.txt"
-        f = open(filepath, "w")
-        f.write("Dane")
-        f.close()
-        pass
-
-
 class View(ttk.Frame):
     root = ttk.Tk()
     # setting window tittle
@@ -45,23 +23,12 @@ class View(ttk.Frame):
     root.maxsize(300, 300)
     root.configure(bg="light yellow")
 
-    def display_checked(self, blue_var=None, red_var=None, yellow_var=None, green_var=None):
-        '''check if the checkbuttons have been toggled, and display
-        a value of '1' if they are checked, '0' if not checked'''
-        red = red_var.get()
-        yellow = yellow_var.get()
-        green = green_var.get()
-        blue = blue_var.get()
+    def display_checked(self):
+        pass
 
-        print("red: {}\nyellow:{}\ngreen: {}\nblue: {}".format(
-            red, yellow, green, blue))
-
-    # Create label
-    label = Label(root, text="gracz1: x                                                                     gracz2:o ")
+# Create label
+    label = Label(root, text="gracz1: X                                                                    gracz2: O")
     label.grid(row=0)
-
-
-
 
 # Create Buttons
     Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=1, column=1)
