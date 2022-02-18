986d0cebfde51f4d13a25ba914c0e37c6dc69cbe
MatGawron <gawmath007@gmail.com>
2022-02-18 13:39:00 +0100

    dodanie przycisków do view

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import numpy as np
+
 #import re
 import tkinter as ttk
+from tkinter import *
 #import tkinter.messagebox
 
 
@@ -33,21 +34,47 @@ class Model:
         f.write("Dane")
         f.close()
         pass
+
+
 class View(ttk.Frame):
     root = ttk.Tk()
-# setting window tittle
+    # setting window tittle
     root.title("Kółko i krzyżyk")
-# setting default window size
+    # setting default window size
     root.minsize(300, 300)
     root.maxsize(300, 300)
     root.configure(bg="light yellow")
 
-    def __init__grid(self, grid=np.ones((3, 3)) * np.nan, gui=None):
-        self.grid = grid
-        self.gui = gui
+    def display_checked(self, blue_var=None, red_var=None, yellow_var=None, green_var=None):
+        '''check if the checkbuttons have been toggled, and display
+        a value of '1' if they are checked, '0' if not checked'''
+        red = red_var.get()
+        yellow = yellow_var.get()
+        green = green_var.get()
+        blue = blue_var.get()
 
-    def __init(self, parent):
-        super().__init__(parent)
+        print("red: {}\nyellow:{}\ngreen: {}\nblue: {}".format(
+            red, yellow, green, blue))
+
+    # Create label
+    label = Label(root, text="gracz1: x                                                                     gracz2:o ")
+    label.grid(row=0)
+
+
+
+
+# Create Buttons
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=1, column=1)
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=1, column=2)
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=1, column=3)
+
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=2, column=1)
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=2, column=2)
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=2, column=3)
+
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=3, column=1)
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=3, column=2)
+    Button(root, text=" ", width=10, height=4, command=display_checked).grid(row=3, column=3)
 
     root.mainloop()
 
